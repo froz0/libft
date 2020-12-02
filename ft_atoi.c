32e95273a646fd2ccaf0abfee023812621a025f0
Théo Matis <tmatis@iMac-de-Theo.local>
2020-12-02 20:01:29 +0100

    fix -1 +1 atoi

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: tmatis <tmatis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/17 18:13:16 by tmatis            #+#    #+#             */
-/*   Updated: 2020/11/18 15:27:48 by tmatis           ###   ########.fr       */
+/*   Updated: 2020/12/02 20:01:13 by tmatis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int		ft_atoi(const char *str)
 {
 	int				sign;
-	unsigned	int	n;
+	long			n;
 
 	sign = 1;
 	n = 0;
@@ -29,5 +29,9 @@ int		ft_atoi(const char *str)
 	}
 	while (*str && ft_isdigit(*str))
 		n = n * 10 + ((*str++) - 48);
+	if (n > 2147483647)
+		return (1);
+	else if (n < 2147483648)
+		return (-1);
 	return (n * sign);
 }

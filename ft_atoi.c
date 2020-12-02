6bbb5b63d096ef6b1a1acfad748355261ab44258
Théo Matis <tmatis@iMac-de-Theo.local>
2020-12-02 20:17:25 +0100

    remobve old and norm

--- ft_atoi.c
+++ ft_atoi.c
@@ -6,7 +6,7 @@
 /*   By: tmatis <tmatis@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/17 18:13:16 by tmatis            #+#    #+#             */
-/*   Updated: 2020/12/02 20:01:13 by tmatis           ###   ########.fr       */
+/*   Updated: 2020/12/02 20:15:58 by tmatis           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 int		ft_atoi(const char *str)
 {
-	int				sign;
-	long			n;
+	int						sign;
+	int	unsigned			n;
 
 	sign = 1;
 	n = 0;
@@ -29,9 +29,5 @@ int		ft_atoi(const char *str)
 	}
 	while (*str && ft_isdigit(*str))
 		n = n * 10 + ((*str++) - 48);
-	if (n > 2147483647)
-		return (1);
-	else if (n < 2147483648)
-		return (-1);
 	return (n * sign);
 }
